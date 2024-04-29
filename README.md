1ee053b916c273d6f1461f103ba63bba2a72ffb9
jose-join <70683227@continental.edu.pe>
2024-04-29 00:35:11 -0500

    CRUD - read cumplido

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # CRUD_angular
 Este es un proyecto CRUD usando angular
 
-1. Se comento la linea, 70 del app.component.html para que corra con normalidad la pagina.
-    <!-- <app-employee-list></app-employee-list> -->
-2. Se agrego estilo css al app.component.html
-    Revisenlo por favor
+1. Se agrego la tabla para visualizar a los empleados que fueron agregados.
+2. Se le agrego estilo.
+3. Se agrego las funciones para los dos botones que falta trabajar
+    (update y delete).
+4. Se agrego un campo de busqueda, donde se realizara la busqueda por Nombre
+5. Agregar el codigo faltante
--- angular-crud/src/app/app.component.ts
+++ angular-crud/src/app/app.component.ts
@@ -1,28 +1,66 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { Employee } from './models/employee';
 import { EmployeeService } from './employee.service';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule], // Asegúrate de incluir FormsModule aquí
+  imports: [RouterOutlet, FormsModule, CommonModule], 
   templateUrl: 'app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  employees: Employee[] = [];
   employee: Employee = {
-    id: 1,
+    id: 0,
     name: 'John Doe',
     country: 'USA'
   };
+  nextId: number = 1;
+  searchTerm: string = '';
+  filteredEmployees: Employee[] = [];
 
   constructor(private employeeService: EmployeeService) { }
 
+  ngOnInit() {
+    this.loadEmployees();
+  }
+
+  loadEmployees() {
+    this.employees = this.employeeService.getEmployees();
+    if (this.employees.length > 0) {
+      this.nextId = Math.max(...this.employees.map(emp => emp.id)) + 1;
+    }
+    this.filteredEmployees = [...this.employees];
+  }
+  
   addEmployee(): void {
+    this.employee.id = this.nextId;
     this.employeeService.addEmployee(this.employee);
-    this.employee = { id: 0, name: '', country: '' }; // Limpiar los campos después de agregar
+    this.employee = { id: 0, name: '', country: '' };
+    this.nextId++;
+    this.loadEmployees();
+  }
+
+  editEmployee(emp: Employee): void {
+  }
+
+  deleteEmployee(emp: Employee): void {
+  }
+  searchEmployee(): void {
+    if (!this.searchTerm.trim()) {
+      this.filteredEmployees = [...this.employees];
+      return;
+    }
+  
+    this.filteredEmployees = this.employees.filter(emp =>
+      emp.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  
+    this.employees = [...this.filteredEmployees];
   }
 }
